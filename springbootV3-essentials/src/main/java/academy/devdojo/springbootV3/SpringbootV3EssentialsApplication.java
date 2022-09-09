862d98d21b4449b3718f981acbe309c20c6a3539
Milton <miltonmjcp@gmail.com>
2022-09-09 15:48:28 -0300

    [main] chore: Createad configuration of project

---
+++ springbootV3-essentials/src/main/java/academy/devdojo/springbootV3/SpringbootV3EssentialsApplication.java
@@ -0,0 +1,13 @@
+package academy.devdojo.springbootV3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootV3EssentialsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootV3EssentialsApplication.class, args);
+	}
+
+}
---
+++ springbootV3-essentials/src/test/java/academy/devdojo/springbootV3/SpringbootV3EssentialsApplicationTests.java
@@ -0,0 +1,13 @@
+package academy.devdojo.springbootV3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootV3EssentialsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
