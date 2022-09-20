7868131ae93c8ce1d39013ee6602c17b22e0a883
Milton <miltonmjcp@gmail.com>
2022-09-20 16:34:05 -0300

    [docker/jpa] chore/feat: Configureted docker, JPA and implements modify of Anime class.

--- springbootV3-essentials/src/main/java/academy/devdojo/springbootV3/domain/Anime.java
+++ springbootV3-essentials/src/main/java/academy/devdojo/springbootV3/domain/Anime.java
@@ -2,12 +2,22 @@ package academy.devdojo.springbootV3.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 //Define get/set/equals and hashcode/toString
 @Data
 //Define que os atributos serão add no constructor
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Anime {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 }
