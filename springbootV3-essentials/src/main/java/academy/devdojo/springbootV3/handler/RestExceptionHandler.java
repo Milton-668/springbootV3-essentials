368145cba1571cea3ae282a1e25490f8c0d706bc
Milton <miltonmjcp@gmail.com>
2022-10-05 17:50:49 -0300

    [overrideHandler] fix: Classe RestExceptionHandler sobreescrita apartir da ResponseEntityExceptionHandler

--- springbootV3-essentials/src/main/java/academy/devdojo/springbootV3/handler/RestExceptionHandler.java
+++ springbootV3-essentials/src/main/java/academy/devdojo/springbootV3/handler/RestExceptionHandler.java
@@ -2,14 +2,19 @@ package academy.devdojo.springbootV3.handler;
 
 import academy.devdojo.springbootV3.exception.BadRequestException;
 import academy.devdojo.springbootV3.exception.BadRequestExceptionDetails;
+import academy.devdojo.springbootV3.exception.ExceptionDetails;
 import academy.devdojo.springbootV3.exception.ValidationExceptionDetails;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.Nullable;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -17,14 +22,10 @@ import java.util.stream.Collectors;
 
 @Log4j2
 @ControllerAdvice
-public class RestExceptionHandler {
-    /*Classe utilizada para personalizar uma exception quando for uma BadRequestException*/
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
-    /*Método responsável por personalizar a exception apresentada no stackTrace, onde que será
-     * retornado uma ResponseEntity com o contéudo dos atributos da classe BadRequestExceptionDetails
-     * e por fim o seu status -> BAD_REQUEST*/
     @ExceptionHandler(BadRequestException.class)
-    public ResponseEntity<BadRequestExceptionDetails> handlerBadRequestException(BadRequestException bre) {
+    public ResponseEntity<BadRequestExceptionDetails> handleBadRequestException(BadRequestException bre) {
         return new ResponseEntity<>(
                 BadRequestExceptionDetails.builder()
                         .timestamp(LocalDateTime.now())
@@ -34,21 +35,16 @@ public class RestExceptionHandler {
                         .developerMessage(bre.getClass().getName())
                         .build(), HttpStatus.BAD_REQUEST);
     }
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
 
-    /*Nesse method há a customização do handler quando a Expcetion lançada for a seguinte:
-     * MethodArgumentNotValidException, então é retornado um ValidationExceptionDetails que herda
-     * da BadRequestExceptionDetails alguns atribtuso e mostrando a mensagem no stacktrace*/
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ValidationExceptionDetails> handlerMethodArgumentNotValidException(
-            MethodArgumentNotValidException exception) {
-        log.info("Field(s) {}", exception.getBindingResult().getFieldError().getField());
-        //Cria uma lista de FieldError, a qual será percorrida para encontrar os resutlados de erros em cima dos campos
-        List<FieldError> fieldErrors = exception.getBindingResult().getFieldErrors();
-        /*Faz uma varredura em cima do campo field, delimitando com uma vírgula para inserir o outro campo*/
+        log.info("Field(s) {}", ex.getBindingResult().getFieldError().getField());
+
+        List<FieldError> fieldErrors = ex.getBindingResult().getFieldErrors();
         String fields = fieldErrors.stream()
                 .map(FieldError::getField)
                 .collect(Collectors.joining(", "));
-        /*Faz uma varredura em cima do campo fieldsMessage, delimitando com uma vírgula para inserir o outro campo*/
         String fieldsMessage = fieldErrors.stream()
                 .map(FieldError::getDefaultMessage)
                 .collect(Collectors.joining(","));
@@ -59,9 +55,23 @@ public class RestExceptionHandler {
                         .status(HttpStatus.BAD_REQUEST.value())
                         .title("Bad Request Exception, Checked the Documentation")
                         .details("Check the field(s) error")
-                        .developerMessage(exception.getClass().getName())
+                        .developerMessage(ex.getClass().getName())
                         .fields(fields)
                         .fieldsMessage(fieldsMessage)
                         .build(), HttpStatus.BAD_REQUEST);
+    }
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(
+            Exception ex, @Nullable Object body, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        ExceptionDetails exceptionDetails = ExceptionDetails.builder()
+                .timestamp(LocalDateTime.now())
+                .status(HttpStatus.BAD_REQUEST.value())
+                .title(ex.getCause().getMessage())
+                .details(ex.getMessage())
+                .developerMessage(ex.getClass().getName())
+                .build();
+
+        return new ResponseEntity<>(exceptionDetails, headers, status);
     }
 }
